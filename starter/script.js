dbe9bdaf611aaf1f44847c2c8b5ed53111cbd8e7
G-M-A-N <gmanngesh@gmail.com>
2022-09-15 01:31:57 +0300

    implemented event listener to rolldice button

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const score0EL = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
-const btnNewGame = document.querySelector('.btn btn--new');
-const btnRollDice = document.querySelector('.btn btn--roll');
-const btnHold = document.querySelector('.btn btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-// diceEl.classList.add('hidden');
+diceEl.classList.add('hidden');
+
+//button Rolling
+
+btnRollDice.addEventListener('click' ,function (){
+    Math.random(Math.)
+})
