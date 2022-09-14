0ddef2da000af7a8d7045f7545d24e6cb60df4de
G-M-A-N <gmanngesh@gmail.com>
2022-09-14 03:33:24 +0300

    DOM 'dice' selection

--- starter/script.js
+++ starter/script.js
@@ -2,6 +2,7 @@
 
 const score0EL = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
 score0EL.textContent = 0;
 score1El.textContent = 0;
