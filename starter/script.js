87a5450274808831a1efa64f610ce0b796c5ad8a
G-M-A-N <gmanngesh@gmail.com>
2022-09-14 17:27:06 +0300

    creeated DOM elements for all three buttons

--- starter/script.js
+++ starter/script.js
@@ -3,8 +3,10 @@
 const score0EL = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn btn--new');
+const btnRollDice = document.querySelector('.btn btn--roll');
+const btnHold = document.querySelector('.btn btn--hold');
 
-score0EL.textContent = 0;
 score1El.textContent = 0;
 
-diceEl.classList.add('hidden');
+// diceEl.classList.add('hidden');
