f0a0776d6615751d7d4231a5fc3f341d681eb4e0
G-M-A-N <gmanngesh@gmail.com>
2022-09-15 09:34:22 +0300

    Linked DOM  random 'dice' to html 'src' dice

--- starter/script.js
+++ starter/script.js
@@ -15,5 +15,8 @@ diceE.classList.add('hidden');
 //button Rolling
 
 btnRollDice.addEventListener('click', function () {
-  Math.trunc(Math.random() * 6);
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  diceE.classList.remove('hidden');
+  diceE.src = `dice-${dice}.png`;
+  console.log(dice);
 });
