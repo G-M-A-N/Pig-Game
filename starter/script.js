7f59128945667a4c3430bf3437b10021e30f732d
G-M-A-N <gmanngesh@gmail.com>
2022-09-15 08:00:35 +0300

    math dice roll using random and trunc methods

--- starter/script.js
+++ starter/script.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const score0El = document.getElementById('score--0');
-const score1El = document.querySelector('#score--1');
-const diceEl = document.querySelector('.dice');
+const score0 = document.getElementById('score--0');
+const score1 = document.querySelector('#score--1');
+const diceE = document.querySelector('.dice');
 const btnNewGame = document.querySelector('.btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
+score0.textContent = 0;
+score1.textContent = 0;
 
-diceEl.classList.add('hidden');
+diceE.classList.add('hidden');
 
 //button Rolling
 
-btnRollDice.addEventListener('click' ,function (){
-    Math.random(Math.)
-})
+btnRollDice.addEventListener('click', function () {
+  Math.trunc(Math.random() * 6);
+});
