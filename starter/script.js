3ddd9fdecb3b2c8994ee73b64d30245cb43a3cca
G-M-A-N <gmanngesh@gmail.com>
2022-09-15 09:43:43 +0300

    declaration of currentScore and apending += dice

--- starter/script.js
+++ starter/script.js
@@ -12,11 +12,15 @@ score1.textContent = 0;
 
 diceE.classList.add('hidden');
 
+let currentScore = 0;
+
 //button Rolling
 
 btnRollDice.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6 + 1);
   diceE.classList.remove('hidden');
   diceE.src = `dice-${dice}.png`;
-  console.log(dice);
+  if (dice !== 1) {
+    currentScore += dice;
+  }
 });
